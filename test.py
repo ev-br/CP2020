5c6f8c7b9f09c24864ca940bbfc80d3896d19636
f3ss1 <n3ffar1an@gmail.com>
2020-03-22 18:02:03 +0300

    UPD: week 5 fixed

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
+import networkx as nx
+import re
+
 import numpy as np
-for i in range(0):
-    print(1)
+
+g = nx.Graph()
+
+
+s = input()
+s = re.split(" ", s)
+array = []
+
+for i in range(0, len(s), 2):
+    array.append ((s[i], s[i + 1]))
+
+g.add_edges_from(array)
+ans = nx.number_connected_components(g)
+print(ans)
+
