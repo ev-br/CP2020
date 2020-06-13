d04d8de76d3b30926edd67055ee748b0f600bf74
f3ss1 <n3ffar1an@gmail.com>
2020-06-13 23:59:21 +0300

    Week 10 added.

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import numpy as np
 from scipy.optimize import curve_fit
 
-a = np.random.uniform(-1, 1, (2, 3))
-b = np.zeros(3)
+def ddbas(x,n):
+    if n==0:
+        return 0.
+    else:
+        return (-2 + n)*(-1 + n)*(-2 + x)*(-1 + x)*(-1 + 1/n + x)**(-3 + n) + 2*(-1 + 1/n + x)**(-1 + n) + 2*(-1 + n)*(-1 + 1/n + x)**(-2 + n)*(-3 + 2*x)
 
-for i in range(2):
-    b[i] += a[i, :] ** 2
-print(b)
+def dif_dif_basis(x, n):
+    if n == 0:
+        return 0
+    else:
+        return (x - 2) * (x - 1) * (n - 1) * (n - 2) * (x - 1 + 1 / n) ** (n - 3) + 2 * (x - 1 + 1 / n) ** (n - 1) + (n - 1) * (x - 1 + 1 / n) ** (n - 2) * (2 * x - 3)
+    
+print(np.linalg.norm(ddbas(np.linspace(0, 1, 100), 5) - dif_dif_basis(np.linspace(0, 1, 100), 5)))
